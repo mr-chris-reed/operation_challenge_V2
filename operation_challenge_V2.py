c4456f5a7b2abba4a440d468fd213c18d8ebda0a
mr-reed <chrree@bhasd.org>
2023-09-26 05:20:35 -0400

    first commit

---
+++ operation_challenge_V2.py
@@ -0,0 +1,3 @@
+"""
+You will write a console program that asks the user for two integers.  The user will supply two integers.  Your program should perform the following operations on the two integers: + , -, *, /, and %.  Your program should neatly and clearly output calculations in a meaningful way to the user
+"""
